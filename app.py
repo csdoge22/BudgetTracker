c39f9997cd007c16b10bf8a3b5ed9c85831c231f
csdoge22 <sunnyambashta@gmail.com>
2024-08-24 23:30:12 -0400

    Made the login and signup interface. Next step will be configuring dbs for users already registered

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, url_for
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "testing 123"
+def signup():
+    return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/home-page', methods=['GET','POST'])
+def home():
+    user = request.form.get('user')
+    if(request.method=='POST'):
+        return render_template("user.html", user=user)
+    else:
+        return redirect(url_for('signup'))
+    
 if(__name__=='__main__'):
     app.run(debug=True)
